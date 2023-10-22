fd40e8c82b369f08441d19445992b14e658300ed
ciu <112779139+kangciu@users.noreply.github.com>
2023-10-22 16:59:02 +0900

    MeethingHeaderView 시작

---
+++ Scrumdinger/Views/MeetingHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingHeaderView.swift
+//  Scrumdinger
+//
+//  Created by 강치우 on 10/22/23.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingHeaderView()
+}
