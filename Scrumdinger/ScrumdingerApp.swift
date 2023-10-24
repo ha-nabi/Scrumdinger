a39ff63543f44539db3a6c1cca2659da62dd5b9b
ciu <112779139+kangciu@users.noreply.github.com>
2023-10-25 01:36:01 +0900

    Persisting data section 5,6 커밋 수정

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -11,7 +11,7 @@ struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
             ScrumsView(scrums: $store.scrums) {
-                Task {
+                Task { // 현재, 파일 시스템에 쓰는 오류가 발생하면 앱이 종료.
                     do {
                         try await store.save(scrums: store.scrums)
                     } catch {
@@ -20,7 +20,7 @@ struct ScrumdingerApp: App {
                 }
             }
             .task { // 작업 수정자가 비동기 함수 호출을 허용한다는 것을 기억하기.
-                do { // 나중에 튜토리얼에서 사용자에게 오류 정보를 제시함.
+                do {
                     try await store.load()
                 } catch {
                     fatalError(error.localizedDescription)
--- Scrumdinger/Views/ScrumsView.swift
+++ Scrumdinger/Views/ScrumsView.swift
@@ -40,6 +40,6 @@ struct ScrumsView: View {
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: .constant(DailyScrum.sampleData))
+        ScrumsView(scrums: .constant(DailyScrum.sampleData), saveAction: {})
     }
 }
